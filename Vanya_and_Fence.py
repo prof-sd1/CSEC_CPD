e6d036b90b57687673f30d2b74ae5feb1663e52e
Samuel <142357575+prof-sd1@users.noreply.github.com>
2025-02-12 00:47:53 +0300

    Vanya_and_Fence.py

--- Vanya_and_Fence.py
+++ Vanya_and_Fence.py
@@ -1,12 +1,12 @@
-dyef min_road_width(n, h, heights):
+def min_road_width(n, h, heights):
     width = 0
     for height in heights:
         if height <= h:
             width += 1
         else:
-            width +=3
+            width += 2
     return width
-n, h = map(int, input("Enter number of friends and the height of the fence: " ).split())
-heights = list(map(int, input("Enter the height of the i-th person: ").split()))
-
+n, h = map(int, input().split())
+heights = list(map(int, input().split()))
+       
 print(min_road_width(n, h, heights))
